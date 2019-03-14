e9760a85cf295ebd75bd27ab32eff8a7743d0d04
talyh <taly.hotimsky@gmail.com>
2019-03-14 16:57:40 -0400

    Added conditional buttons based on grid size

--- app/pods/components/product-grid/component.js
+++ app/pods/components/product-grid/component.js
@@ -2,12 +2,22 @@ import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
+    size: "small",
+    customActions: {
+        small: {
+            "Expand": function () { this.get('router').transitionTo('products.show', this.productID); },
+        },
+        large: {
+            "Coverage": () => console.log("coverage"),
+            "Download": () => console.log("download")
+        }
+    },
     title: null,
     data: null,
     footer: null,
     productID: null,
     router: service(),
-    customActions: {
-        "Expand": function () { this.get('router').transitionTo('products.show', this.productID); }
-    },
+    didRender() {
+        // console.log(this.customActions.chosen)
+    }
 });
